ac4eeb26a3b918732b89d555595aa79ccdbb56aa
itsmearion <bilqiss0893@gmail.com>
2025-04-22 21:34:51 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+API_ID = 12345678              # Isi API_ID kamu
+API_HASH = "abcdef1234567890abcdef1234567890"  # Isi API_HASH kamu
+BOT_TOKEN = "ISI_TOKEN_BOT"     # Isi Token Bot kamu
+ADMIN_GROUP_ID = -1001234567890 # ID Grup Admin (pakai minus)
